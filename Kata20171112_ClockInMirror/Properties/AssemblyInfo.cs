f70a735ef2341ea94874bf4a86cf8f942d56c8ad
Dustin Liu Chao-Ling <dustin.liu62@titansoft.com.sg>
2017-11-12 15:47:37 +0800

    加入專案檔案。

---
+++ Kata20171112_ClockInMirror/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata20171112_ClockInMirror")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kata20171112_ClockInMirror")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6dcf1f9c-9b19-42b8-9f46-7949995d9b9f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
